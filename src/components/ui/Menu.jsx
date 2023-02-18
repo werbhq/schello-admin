540f4b5260f64eca20a79c62c0aee2a2096db531
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-19 02:32:36 +0530

    add: community video and article to submenu

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -1,17 +1,24 @@
-// in src/MyMenu.js
 import { Menu } from "react-admin";
-import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
 import { MAPPING } from "../../provider/mapping";
+import SubMenu from "./SubMenu";
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
+import GroupsIcon from "@mui/icons-material/Groups";
 
-export const CustomMenu = () => (
-  <Menu>
-    <Menu.DashboardItem />
-    <Menu.ResourceItem name={MAPPING.DRUG_REPORTS} />
-    <Menu.ResourceItem name={MAPPING.EVENTS} />
-    <Menu.Item
-      to="/crime-mapping"
-      primaryText="Crime Mapping"
-      leftIcon={<LocalPoliceIcon />}
-    />
-  </Menu>
-);
+export const CustomMenu = () => {
+  return (
+    <Menu>
+      <Menu.DashboardItem />
+      <Menu.ResourceItem name={MAPPING.DRUG_REPORTS} />
+      <Menu.ResourceItem name={MAPPING.EVENTS} />
+      <SubMenu name="Community" icon={<GroupsIcon />}>
+        <Menu.ResourceItem name={MAPPING.COMMUNITY.ARTICLE} />
+        <Menu.ResourceItem name={MAPPING.COMMUNITY.VIDEO} />
+      </SubMenu>
+      <Menu.Item
+        to="/crime-mapping"
+        primaryText="Crime Mapping"
+        leftIcon={<LocalPoliceIcon />}
+      />
+    </Menu>
+  );
+};
