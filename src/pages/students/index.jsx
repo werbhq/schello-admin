59a8037e8b4c6a692054b261a33061d7d35176d2
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-19 23:36:28 +0530

    temp: add students

--- src/pages/students/index.jsx
+++ src/pages/students/index.jsx
@@ -1,10 +1,10 @@
 import icon from "@mui/icons-material/School";
 
 import { MAPPING } from "../../provider/mapping";
-import AuthorizedTeacherList from "./List";
-import AuthorizedTeacherShow from "./Show";
-import AuthorizedTeacherEdit from "./Edit";
-import AuthorizedTeacherCreate from "./Create";
+// import AuthorizedTeacherList from "./List";
+// import AuthorizedTeacherShow from "./Show";
+// import AuthorizedTeacherEdit from "./Edit";
+// import AuthorizedTeacherCreate from "./Create";
 import { ListGuesser, ShowGuesser } from "ra-ui-materialui";
 
 const Students = {
