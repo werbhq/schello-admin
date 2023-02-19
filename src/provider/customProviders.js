2f2e54819943db53eeb30d061ac5e74bfe90466a
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-19 21:28:10 +0530

    disable: attendo providers

--- src/provider/customProviders.js
+++ src/provider/customProviders.js
@@ -1,19 +1,19 @@
-import { ClassroomProvider } from "./custom/classroom";
-import { StudentsProvider } from "./custom/students";
-import { SubjectsProvider } from "./custom/subjects";
-import { SemestersProvider } from "./custom/semesters";
-import { AuthTeachersProvider } from "./custom/authorizedTeachers";
+// import { ClassroomProvider } from "./custom/classroom";
+// import { StudentsProvider } from "./custom/students";
+// import { SubjectsProvider } from "./custom/subjects";
+// import { SemestersProvider } from "./custom/semesters";
+// import { AuthTeachersProvider } from "./custom/authorizedTeachers";
+// import { ReportsProvider } from "./custom/reports";
 import { DrugReportsProvider } from "./custom/drug_reports";
-import { ReportsProvider } from "./custom/reports";
 
 // ADD YOUR PROVIDERS HERE
 const CustomProviders = [
-  ClassroomProvider,
-  SemestersProvider,
-  StudentsProvider,
-  SubjectsProvider,
-  AuthTeachersProvider,
-  ReportsProvider,
+  // ClassroomProvider,
+  // SemestersProvider,
+  // StudentsProvider,
+  // SubjectsProvider,
+  // AuthTeachersProvider,
+  // ReportsProvider,
   DrugReportsProvider,
 ];
 
