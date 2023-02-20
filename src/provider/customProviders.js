0aa6de66b17d7be9dadea7d0a86a1f05d230b75d
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-20 23:44:37 +0530

    remove: attendo dependencies

--- src/provider/customProviders.js
+++ src/provider/customProviders.js
@@ -1,20 +1,6 @@
-// import { ClassroomProvider } from "./custom/classroom";
-// import { StudentsProvider } from "./custom/students";
-// import { SubjectsProvider } from "./custom/subjects";
-// import { SemestersProvider } from "./custom/semesters";
-// import { AuthTeachersProvider } from "./custom/authorizedTeachers";
-// import { ReportsProvider } from "./custom/reports";
 import { DrugReportsProvider } from "./custom/drug_reports";
 
 // ADD YOUR PROVIDERS HERE
-const CustomProviders = [
-  // ClassroomProvider,
-  // SemestersProvider,
-  // StudentsProvider,
-  // SubjectsProvider,
-  // AuthTeachersProvider,
-  // ReportsProvider,
-  DrugReportsProvider,
-];
+const CustomProviders = [DrugReportsProvider];
 
 export default CustomProviders;
--- src/provider/mapping.js
+++ src/provider/mapping.js
@@ -1,13 +1,4 @@
 export const MAPPING = {
-  // SUBJECT: "subjects",
-  // CLASSROOMS: "classrooms",
-  // STUDENTS: "classrooms/students",
-  // SEMESTERS: "semesters",
-  // DATA: "data",
-  // AUTH_TEACHERS: "authorized_teachers",
-  // MASTER_CLASSROOMS: "master_classrooms",
-  // REPORTS: "reports",
-
   DRUG_REPORTS: "reports",
   COMMUNITY: {
     ARTICLE: "community-article",
