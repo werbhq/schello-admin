6070f65b9bbb02f3370a540dbfb2244268f10f99
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-24 22:36:32 +0530

    change: base url path of api

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://us-central1-merit-werb.cloudfunctions.net/serverCall"
-    : "http://localhost:5001/merit-werb/us-central1/serverCall";
+    ? "https://us-central1-merit-werb.cloudfunctions.net/api"
+    : "http://localhost:5001/merit-werb/us-central1/api";
 
 export const baseApi = axios.create({
   baseURL,
