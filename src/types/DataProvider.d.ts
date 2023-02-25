9d0d11613fe052604cb8c08014125d8522d31a26
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-02-25 10:21:12 +0530

    fix: dataprovider types

--- src/types/DataProvider.d.ts
+++ src/types/DataProvider.d.ts
@@ -1,4 +1,24 @@
-import { DataProvider } from "react-admin";
+import {
+  CreateParams,
+  CreateResult,
+  DataProvider,
+  DeleteManyParams,
+  DeleteParams,
+  DeleteResult,
+  GetListResult,
+  GetManyParams,
+  GetManyReferenceParams,
+  GetManyReferenceResult,
+  GetManyResult,
+  GetOneParams,
+  GetOneResult,
+  UpdateManyParams,
+  UpdateManyResult,
+  UpdateParams,
+  UpdateResult,
+  DeleteManyResult,
+  GetListParams,
+} from "react-admin";
 
 export interface DataProviderCustom extends DataProvider {
   resource: string;
@@ -6,9 +26,9 @@ export interface DataProviderCustom extends DataProvider {
   delete?: (resource: string, params: DeleteParams) => Promise<DeleteResult>;
   deleteMany?: (
     resource: string,
-    params: DeleteParams
-  ) => Promise<DeleteManyParams>;
-  getList?: (resource: string, params: ListParams) => Promise<GetListResult>;
+    params: DeleteManyParams
+  ) => Promise<DeleteManyResult>;
+  getList?: (resource: string, params: GetListParams) => Promise<GetListResult>;
   getOne?: (resource: string, params: GetOneParams) => Promise<GetOneResult>;
   getMany?: (resource: string, params: GetManyParams) => Promise<GetManyResult>;
   getManyReference?: (
